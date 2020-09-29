21702131c57f40a92d4e80c24d3dd00cb5d97302
Moonsun Hwang <moonsun.hwg@gmail.com>
2020-09-29 21:58:13 +0000

    whichSchool Exercise

---
+++ whichSchool.js
@@ -0,0 +1,15 @@
+const whichSchool  = function (13) {
+if(age < 13) 
+  school = "Elementary School";
+  console.log("You should go to Elementary School");
+} else if (age >= 13 && age <= 18) {
+  school = "Middle School"
+  console.log("You should go to Middle School")
+} else if (age > 18) {
+  school = "Lighthouse Labs"
+  console.log("You should go to Lighthouse Labs")
+}
+
+
+
+console.log(whichSchool(23))
